474c224cb3d843710a59afee310665766cc06b02
jeremytajiri2007-hue <jeremytajiri2007@gmail.com>
2025-11-27 22:52:10 -0800

    Update bankAccount.cpp

--- Inheritance-Activity/bankAccount.cpp
+++ Inheritance-Activity/bankAccount.cpp
@@ -1,1 +1,35 @@
+```cpp
+
+#include "bankAccount.h"
+#include <iostream>
+
+bankAccount::bankAccount() = default;
+
+bankAccount::bankAccount(int accountNumber, double balance)
+    : accountNumber(accountNumber), balance(balance) {}
+
+void bankAccount::setAccountNumber(int accountNumber) {
+    this->accountNumber = accountNumber;
+}
+
+int bankAccount::getAccountNumber() const {
+    return accountNumber;
+}
+
+double bankAccount::getAccountBalance() const {
+    return balance;
+}
+
+void bankAccount::deposit(double amount) {
+    balance += amount;
+}
+
+void bankAccount::withdraw(double amount) {
+    balance -= amount;
+}
+
+void bankAccount::print() const {
+    std::cout << "Account Number: " << accountNumber << std::endl;
+    std::cout << "Balance (USD): " << balance << std::endl;
+}
 
